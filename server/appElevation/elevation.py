91d3b5549929a88972527bb5f70614cf0d6345e3
Sarthak Ubuntu <sjsarthakh@gmail.com>
2020-12-20 01:50:26 +0800

    Pushed final version.

--- server/appElevation/elevation.py
+++ server/appElevation/elevation.py
@@ -61,14 +61,13 @@ def searchForNodes (personLat, personLong):
     searchThresholdLB = 75 #Lower Bound
     searchThresholdUB = 125 #Upper Bound
     overpassCallSearchRadius = 150 #Search Radius
-    elevationThreshold = 0.01 #1m of elevation for 10m of distance
+    elevationThreshold = 0.2 #5m of elevation for 10m of distance
     
-    goodRoutes = [[]] # holds the good routes with elevation data. Each column has lat1, long1, elevation1, lat2, long2, elevation2, total distance, total change in elevation
+    goodRoutes = [] # holds the good routes with elevation data. Each column has lat1, long1, elevation1, lat2, long2, elevation2, total distance, total change in elevation
 
     #API Calls to OverPass for Nodes. Need to store lat and long
-    minLat, minLon, maxLat, maxLon = boundingBox((39.903344, -75.346205), 100)
+    minLat, minLon, maxLat, maxLon = boundingBox((personLat, personLong), overpassCallSearchRadius)
     nodes  = getAllPossibleWays(minLat[0], minLon[1], maxLat[0], maxLon[1]) #When Parsing in, use the overpassCallSearchRadius
-    #nodes[0][0].insert(0, 10) #importation into this array
     
     #API Calls to Roads API to snap nodes back onto road and then get elevation
     for node in nodes:
@@ -101,4 +100,6 @@ def searchForNodes (personLat, personLong):
                             goodRoutes.append([first[0], first[1], first[2], second[0], second[1], second[2], dis, elevation])
     return goodRoutes
 
-print(len(searchForNodes(1,1)))
+result = searchForNodes(37.702514, -122.455611)
+print(len(result))
+print(result)
