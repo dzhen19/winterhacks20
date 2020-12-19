0e0cfbbf87b015f6211b763a6cbe9cc4188cf302
Sarthak Ubuntu <sjsarthakh@gmail.com>
2020-12-19 20:04:40 +0800

    Updated elevation.py

--- server/appElevation/elevation.py
+++ server/appElevation/elevation.py
@@ -1,17 +1,32 @@
 import googlemaps as gm
+import math
     
 from array import *
     
 gmaps = gm.Client(key="AIzaSyAs7JlLi3pn-VYlltsDfVwWS9J8AhbeM3U") #Key for API
-    
+
+def distanceBetweenTwoPoints(a, b):
+    lat1 = (a[0]/180) * math.pi
+    lat2 = (b[0]/180) * math.pi
+    lon1 = (a[1]/180) * math.pi
+    lon2 = (b[1]/180) * math.pi
+    R = 6371
+    x = (lat2 - lat1) * math.cos((lon1+lon2)/2)
+    print(math.cos((lon1+lon2)/2))
+    y = (lon2 - lon1)
+    distance = R * math.sqrt((x*x)+(y*y))
+    return distance
+
 def searchForNodes (personLat, personLong):
     searchThresholdLB = 75 #Lower Bound
-    searchThresholdUP = 125 #Upper Bound
+    searchThresholdUB = 125 #Upper Bound
     overpassCallSearchRadius = 150 #Search Radius
     elevationThreshold = 0.1 #1m of elevation for 10m of distance
     
+    goodRoutes = [[]] # holds the good routes with elevation data. Each column has lat1, long1, elevation1, lat2, long2, elevation2, total distance, total change in elevation
+
     #API Calls to OverPass for Nodes. Need to store lat and long
-    nodes  = [[[]]]
+    nodes  = [[[]]] #When Parsing in, use the overpassCallSearchRadius
     #nodes[0][0].insert(0, 10) #importation into this array
     
     #API Calls to Roads API to snap nodes back onto road and then get elevation
@@ -25,6 +40,27 @@ def searchForNodes (personLat, personLong):
             latLong = (c[0], c[1])
             c[2] = gmaps.elevation(latLong)[0]["elevation"]
     
-    return lat
-    
-print(searchForNodes(2, 2))
+    for node in nodes:
+        for firstComparison in node:
+            for secondComparison in node:
+                dis = distanceBetweenTwoPoints(firstComparison, secondComparison)
+                if dis <= searchThresholdUB and dis >= searchThresholdLB:
+                    elevation = firstComparison[2] - secondComparison[2]
+                    first = firstComparison
+                    second = secondComparison
+                    if elevation < 0:
+                        elevation = elevation * -1
+                        third = second
+                        second = first
+                        first = third
+                    if elevation/dis > elevationThreshold:
+                        #Perform API Call on google routes to see if Dis is approximately the same
+                        #If it is approx the same, add to good routes table
+
+    return goodRoutes
+
+thisTuple1 = (1.2957577, 103.878395)
+thisTuple2 = (1.297772, 103.880737)
+print(distanceBetweenTwoPoints(thisTuple1, thisTuple2))
+
+
